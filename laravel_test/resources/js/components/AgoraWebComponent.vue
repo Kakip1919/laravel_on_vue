51acfd98ef449f819f00a57b5921f34fe2163fda
*Veld9Sm6Tg <kakuta.atsushi@outward.jp>
2022-03-17 14:54:14 +0900

    first commit avoid files

---
+++ laravel_test/resources/js/components/AgoraWebComponent.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="hello">
+    <h1>
+      Video Call<br><small style="font-size: 14pt;">Powered by Agora.io</small>
+    </h1>
+    <h4>Local video</h4>
+    <div id="me"></div>
+    <h4>Remote video</h4>
+    <div id="container"></div>
+  </div>
+</template>
+
+<script>
+import AgoraRTC from 'agora-rtc-sdk'
+
+export default {
+  name: 'agora_web_component',
+
+  created() {
+    let client = AgoraRTC.createClient({
+      mode: "rtc",
+      codec: "vp8",
+    });
+
+    let localStream = AgoraRTC.createStream({
+      audio: true,
+      video: true,
+    });
+
+    client.init("aaac7624c64348699a189ddc728ba18f");
+
+    client.join("006aaac7624c64348699a189ddc728ba18fIAD8AOjkPqTuF8NynFf1461M/9QQM8snCV24Qg2BQgz3PHe3Y70AAAAAEADjTvSOdboyYgEAAQB0ujJi", "demko", null, () => {
+      localStream.init(() => {
+        localStream.play("me");
+        client.publish(localStream, handleError);
+      }, handleError);
+      client.on("stream-added", function (evt) {
+        client.subscribe(evt.stream, handleError);
+      });
+      client.on("stream-subscribed", function (evt) {
+        let stream = evt.stream;
+        let streamId = String(stream.getId());
+        addVideoStream(streamId);
+        stream.play(streamId);
+      });
+      client.on("stream-removed", function (evt) {
+        let stream = evt.stream;
+        let streamId = String(stream.getId());
+        stream.close();
+        removeVideoStream(streamId);
+      });
+      client.on("peer-leave", function (evt) {
+        let stream = evt.stream;
+        let streamId = String(stream.getId());
+        stream.close();
+        removeVideoStream(streamId);
+      });
+    }, handleError);
+  }
+}
+
+const handleError = function (err) {
+  console.log("Error: ", err);
+};
+
+function addVideoStream(elementId) {
+  let streamDiv = document.createElement("div");
+  streamDiv.id = elementId;
+  streamDiv.style.transform = "rotateY(180deg)";
+  streamDiv.style.width = "400px";
+  streamDiv.style.height = "400px";
+  streamDiv.style.margin = "0 auto";
+
+  document.getElementById("container").appendChild(streamDiv);
+}
+
+function removeVideoStream(elementId) {
+  let remoteDiv = document.getElementById(elementId);
+  if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+}
+</script>
+
+<style scoped>
+* {
+  font-family: sans-serif;
+}
+
+h1, h4 {
+  text-align: center;
+}
+
+#me {
+  width: 400px;
+  height: 400px;
+  margin: 0 auto;
+}
+#container video {
+  position: relative !important;
+}
+</style>
